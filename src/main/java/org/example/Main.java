c0f5134c05fe896e683aa2cbad638c3613efa246
Alain Kermis <alain.kermis@sonarsource.com>
2023-10-23 16:38:26 +0200

    DEV Init

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,29 @@
 package org.example;
 
+import java.io.UnsupportedEncodingException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import javax.crypto.Cipher;
+import javax.crypto.NoSuchPaddingException;
+
 public class Main {
   public static void main(String[] args) {
     System.out.println("Hello world!");
+
+    try {
+      Cipher des = Cipher.getInstance("DES"); // Noncompliant
+
+      SecureRandom sr = new SecureRandom();
+      sr.setSeed(123456L); // Noncompliant
+      int v = Math.toIntExact(sr.nextLong());
+
+      sr = new SecureRandom("abcdefghijklmnop".getBytes("us-ascii")); // Noncompliant
+      v = Math.toIntExact(sr.nextLong());
+
+    } catch(NoSuchAlgorithmException | NoSuchPaddingException e) {
+      // ...
+    } catch (UnsupportedEncodingException e) {
+      throw new RuntimeException(e);
+    }
   }
 }

a5fafacb4b3a142ebcc3cc8745d5458e05f30071
Alain Kermis <alain.kermis@sonarsource.com>
2023-10-25 08:44:48 +0200

    DEV-3 Init

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,17 @@
 package org.example;
 
+import java.security.NoSuchAlgorithmException;
+import javax.crypto.Cipher;
+import javax.crypto.NoSuchPaddingException;
+
 public class Main {
   public static void main(String[] args) {
     System.out.println("Hello world!");
+
+    try {
+      Cipher des = Cipher.getInstance("DES"); // Noncompliant
+    } catch(NoSuchAlgorithmException | NoSuchPaddingException e) {
+      // ...
+    }
   }
 }

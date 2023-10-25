b5c964da8e26721d41367f235ae5de0dc43add88
Alain Kermis <alain.kermis@sonarsource.com>
2023-10-25 08:35:32 +0200

    DEV-2 Init

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,36 +1,7 @@
 package org.example;
 
-import java.io.UnsupportedEncodingException;
-import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
-import javax.crypto.Cipher;
-import javax.crypto.NoSuchPaddingException;
-
 public class Main {
   public static void main(String[] args) {
     System.out.println("Hello world!");
-
-    try {
-      Cipher des = Cipher.getInstance("DES"); // Noncompliant
-      Cipher des2 = Cipher.getInstance("DES"); // Noncompliant
-      Cipher des3 = Cipher.getInstance("DES"); // Noncompliant
-
-
-      SecureRandom sr = new SecureRandom();
-      sr.setSeed(123456L); // Noncompliant
-      sr.setSeed(123456L); // Noncompliant
-      sr.setSeed(123456L); // Noncompliant
-      sr.setSeed(123456L); // Noncompliant
-      int v = Math.toIntExact(sr.nextLong());
-
-      sr = new SecureRandom("abcdefghijklmnop".getBytes("us-ascii")); // Noncompliant
-      sr = new SecureRandom("abcdefghijklmnop".getBytes("us-ascii")); // Noncompliant
-      v = Math.toIntExact(sr.nextLong());
-
-    } catch(NoSuchAlgorithmException | NoSuchPaddingException e) {
-      // ...
-    } catch (UnsupportedEncodingException e) {
-      throw new RuntimeException(e);
-    }
   }
 }
